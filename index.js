bd46bea1d834ebe5bf18ea6c998eeeb018cd5150
Jesper Svennevid <github@svennevid.se>
2013-05-28 16:16:46 +0200

    Switched to usage of node-config instead of internal configuration support

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+var config = require('config');
+config.setModuleDefaults('appserver', {
+    "web": {
+        "port": 8888,
+        "wsPort": 0
+    },
+    "production": false,
+    "compress": false,
+    "preload": false,
+    "session": {
+        "key": "connect.sid",
+        "secret": "SECRET",
+        "timeout": 48 * (60 * 60) * 1000
+    },
+    "streaming": {
+        "registry": "local",
+        "amqp": {
+            "url": ""
+        }
+    },
+    "memcached": {
+        host: ""
+    }
+});
+
 module.exports = require('./lib/server');
